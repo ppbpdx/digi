35515ad3e27489b7de4c90cdb102d2921d5d49d4
ppbpdx <philip@pridmorebrown.com>
2018-02-19 16:30:07 -0800

    Create pybuild.py

---
+++ pybuild.py
@@ -0,0 +1,18 @@
+path = 'json_files'
+project_list = []
+output_dict= {}
+
+jsonfile_list = [f for f in os.listdir(path)]
+
+for file in jsonfile_list:
+    file_path = f'{path}/{file}'
+    file = json.load(open(file_path))
+    file['label'] = file.pop('project')
+    file['image'] = file.pop('logoURL')
+    project_list.append(file)
+    
+    
+output_dict['elements'] = project_list
+output_file = 'map.json'
+with open(output_file, 'w') as f:
+    json.dump(output_dict, f)
